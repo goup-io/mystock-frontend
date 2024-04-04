1a6a227b41c3e9bef69257da3ce895fd4bc530f8
LccsBueno <luccas.lima@sptech.school>
2024-04-04 11:29:51 -0300

    Subindo algumas mudanças na tela de menu

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -7,13 +7,14 @@ import MenuIcon from '../buttons/buttonMenu.js'
 function Menu(){
     return(
         <section class="flex flex-col items-center h-[100vh] w-full justify-center">
-            <div class="absolute right-4 top-4">
+            <div class="flex flex-row w-full justify-between px-8">
+                <img src={myStockLogo}></img>
                 <Notification></Notification>
             </div>
 
-            <div class="w-10/12 h-4/5 bg-[#355070] shadow px-12 pt-2">
+            <div class="w-10/12 h-4/5 bg-[#355070] shadow px-12 pt-2 overflow-auto ">
                 <Header telaAtual="Menu"></Header>
-                <ul class="flex flex-col items-center gap-7 mt-20 overflow-auto">
+                <ul class="flex flex-col items-center gap-7 mt-20">
                     <ItemLista>ÁREA DE PRÉ-VENDA</ItemLista>
                     <ItemLista>ESTOQUE</ItemLista>
                     <ItemLista>HISTÓRICO DE VENDAS</ItemLista>
