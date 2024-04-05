201475c459d1f8ac9c0136e9ff1826e71b5b77ae
LccsBueno <luccas.lima@sptech.school>
2024-04-05 01:25:00 -0300

    Aumentando o tamanho da tela de menu

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -2,17 +2,16 @@ import Header from '../header/Header.js'
 import myStockLogo from '../../assets/icons/logoMyStock.png'
 import Notification from '../buttons/buttonNotification.js'
 import Footer from '../footer/Footer.js'
-import MenuIcon from '../buttons/buttonMenu.js'
 
 function Menu(){
     return(
         <section class="flex flex-col items-center h-[100vh] w-full justify-center">
-            <div class="flex flex-row w-full justify-between px-8">
+            <div class="flex flex-row w-full justify-between px-8 pl-[5%]">
                 <img src={myStockLogo}></img>
                 <Notification></Notification>
             </div>
 
-            <div class="w-10/12 h-4/5 bg-[#355070] shadow px-12 pt-2 overflow-auto ">
+            <div class="w-[89%] h-4/5 bg-[#355070] shadow px-12 pt-2 overflow-auto ">
                 <Header telaAtual="Menu"></Header>
                 <ul class="flex flex-col items-center gap-7 mt-20">
                     <ItemLista>ÁREA DE PRÉ-VENDA</ItemLista>
