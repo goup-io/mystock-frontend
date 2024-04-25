7f226d5febb94c889033d7e1ea7dbdcaffac8881
LccsBueno <luccas.lima@sptech.school>
2024-04-24 22:58:39 -0300

    Adicionando o HttpModalHandler para lidar com os status http e seus modais

--- src/connections/Validate.js
+++ src/connections/Validate.js
@@ -3,7 +3,7 @@ import Cookie from './Cookie.js'
 export class Validate{
 
     static screenPermission(idPermissionRequired){
-        
+        //Comparar o idPermissionRequired com a permissao de quem logou no sistema
     }
 }
 
