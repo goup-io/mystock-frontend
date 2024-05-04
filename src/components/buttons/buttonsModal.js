f651e95faf3a67c81c7c9f5c6a08e804e9cfcf39
Kauanny Tenorio <kakau24112004@hotmail.com>
2024-05-04 19:55:57 -0300

    redimensionando a tela do caixa

--- src/components/buttons/buttonsModal.js
+++ src/components/buttons/buttonsModal.js
@@ -1,9 +1,11 @@
-function ButtonModal(props){
-    return(
-         <button onClick={props.funcao} class="px-4 font-medium bg-[#355070] rounded text-white ">
-             <span class="text-white text-base">{props.children}</span>
-         </button>
-    )
+function ButtonModal(props) {
+    const cor = props.cor || 'bg-[#355070]';
+
+    return (
+        <button onClick={props.funcao} className={`px-4 font-medium ${cor} rounded text-white`}>
+            <span className="text-white text-base">{props.children}</span>
+        </button>
+    );
 }
 
 export default ButtonModal;
