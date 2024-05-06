19ecaa0599283bd4aba690c1283d06160c3f426b
Kauanny Tenorio <kakau24112004@hotmail.com>
2024-05-06 20:54:02 -0300

    Criando componete dos avisos

--- src/components/pages/aviso/Aviso.js
+++ src/components/pages/aviso/Aviso.js
@@ -4,23 +4,31 @@ import TitleBox from '../../header/TitleBox.js'
 import ChartBox from '../../chartsBoxes/ChartBox.js'
 import Filter from '../../inputs/filter.js'
 import InputSearch from '../../inputs/inputSearch.js'
+import ModalAviso from '../../alerts/ModalAviso.js'
 
-function Relatorio() {
+function Aviso() {
+
+    const avisos = [
+        {tipo: 'Vermelho', titulo: 'Baixo Estoque', descricao: 'O Air max 200 tamanho 45 possuem apenas 5 pares em estoque.', dataHora: '02/05/2024 17:57:12'},
+        {tipo: 'Laranja', titulo: 'Baixo Estoque', descricao: 'O Air max 200 tamanho 45 possuem apenas 50 pares em estoque.', dataHora: '02/05/2024 16:57:12'},
+    ]
 
     return (
         <>
             <PageLayout>
                 <TitleBox title="Mural de Avisos"></TitleBox>
                 <div className='w-full flex md:flex-row md:justify-center rounded-md py-4 px-5 drop-shadow-md items-center text-sm bg-white'>
-                    <Filter modelo cor tamanho preço></Filter>
+                    <Filter modelo cor></Filter>
                 </div>
                 <ChartBox>
                     <div className='p-2 px-3 flex justify-between'>
                         <h2 className='font-medium text-lg'>AVISOS</h2>                        
                         <InputSearch>Pesquisar</InputSearch>
                     </div>
-                    <div>
-                        
+                    <div className='flex flex-col gap-2 px-3 mt-3'>
+                        {avisos.map((aviso, index) => (
+                            <ModalAviso tipo={aviso.tipo} titulo={aviso.titulo} descricao={aviso.descricao} dataHora={aviso.dataHora} />
+                        ))}
                     </div>
                 </ChartBox>
             </PageLayout>
@@ -29,4 +37,4 @@ function Relatorio() {
 
 }
 
-export default Relatorio
+export default Aviso
