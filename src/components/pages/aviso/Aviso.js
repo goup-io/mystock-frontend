5ef7dabe75684773b46be4d2f04ca1f3afe7c0fd
Kauanny Tenorio <kakau24112004@hotmail.com>
2024-05-09 14:28:39 -0300

    Alteração na borda do box da table de estoque

--- src/components/pages/aviso/Aviso.js
+++ src/components/pages/aviso/Aviso.js
@@ -18,7 +18,7 @@ function Aviso() {
             <PageLayout>
                 <TitleBox title="Mural de Avisos"></TitleBox>
                 <div className='w-full flex md:flex-row md:justify-center rounded-md py-4 px-5 drop-shadow-md items-center text-sm bg-white'>
-                    <Filter modelo cor></Filter>
+                    <Filter data cor></Filter>
                 </div>
                 <ChartBox>
                     <div className='p-2 px-3 flex justify-between'>
