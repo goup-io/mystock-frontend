1f6351792f82abe3b7cd645c91fcef804ca8650a
A913506 <enzo.stane@atos.net>
2024-05-10 18:21:47 -0300

    arrumando modais de desconto e historico de vendas

--- src/components/modals/modalAddDiscount.js
+++ src/components/modals/modalAddDiscount.js
@@ -5,44 +5,40 @@ import InputAndLabelModal from "../inputs/inputAndLabelModal";
 import HeaderModal from "../modals/headerModal";
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import ItemSeparadoPorLinhaTracejada from '../tables/ItemSeparadoPorLinhaTracejada'
 
 function ModalDiscount() {
     return (
         <>
-            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[34rem] h-[18.5rem] flex flex-col items-center justify-around  bg-white p-2 rounded-lg border border-black">
-                <div className="w-[32rem]">
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[42rem] h-[22rem] flex flex-col items-center justify-around  bg-white p-2 rounded-lg border border-black">
+                <div className="w-[40rem]">
                     <HeaderModal
                         props="Adicionar Desconto"
                     ></HeaderModal>
                 </div>
-                <div className="w-[32rem] h-[12rem] flex flex-col rounded justify-around p-3 bg-[#F5F3F4] border-solid shadow-[5px_5px_10px_0_rgba(0,0,0,0.14)] border-gray-700">
+                <div className="w-[40rem] h-[14rem] flex flex-col rounded justify-around p-3 bg-[#F5F3F4] border-solid shadow-[5px_5px_10px_0_rgba(0,0,0,0.14)] border-gray-700">
 
                     <div className="flex justify-between mb-4">
                         <InputAndLabelModal
                             type="Text"
-                            placeholder="digite o funcionario..."
+                            placeholder="Digite a porcentagem do desconto..."
                         >Porcentagem(%):</InputAndLabelModal>
                         <InputAndLabelModal
                             type="Text"
-                            placeholder="digite o funcionario..."
+                            placeholder="0,00"
                         >Valor Calculado(R$):</InputAndLabelModal>
                     </div>
 
-                    <div className="w-full h-[0.1rem]  bg-slate-500 mt-2"></div>
-
-                    <div className="w-full h-[1rem]  flex items-center justify-between rounded mt2">
-                        <p className="mr-4 font-medium">Valor Atual:</p>
-                        <p> R$ 230,00</p>
-                    </div>
-
-                    <div className="w-full h-[0.1rem]  bg-slate-500 mt-2"></div>
-
-                    <div className="w-full h-[1rem]  flex items-center justify-between rounded mb-2">
-                        <p className="mr-4 font-medium">Valor Após o Desconto: </p>
-                        <p>R$ 200,00</p>
-                    </div>
+                    <ItemSeparadoPorLinhaTracejada
+                            infoEsquerda={"Valor Atual:"}
+                            infoDireita={"R$ 300,00"}
+                        />
+                        <ItemSeparadoPorLinhaTracejada
+                            infoEsquerda={"Valor pós Desconto:"}
+                            infoDireita={"R$ 300,00"}
+                        />
                 </div>
-                <div className="w-[32rem] flex justify-end h-6 ">
+                <div className="w-[40rem] flex justify-end h-6 ">
                     <ButtonClear>Limpar</ButtonClear>
                     <ButtonModal>Adicionar</ButtonModal>
                 </div>
