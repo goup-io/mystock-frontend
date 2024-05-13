c591063b4b49d1a427403aca6f60a23fa9638088
Kauanny Tenorio <kakau24112004@hotmail.com>
2024-05-12 21:54:15 -0300

    Alterando o style do input de pesquisas

--- src/components/inputs/inputSearchModal.js
+++ src/components/inputs/inputSearchModal.js
@@ -7,7 +7,7 @@ function InputSearcModal(props) {
         
             <div className="flex items-center">
                 <p class="form-floating text-lg text-black ' mr-2 font-normal">{props.children}:</p>
-                <input type={`${props.type}`} class="w-[12rem] h-6 rounded-l-lg bg-[F5F3F4] pl-2 text-[1rem] font-[400] text-[#555] form-control border-2 border-gray-700"
+                <input type={`${props.type}`} class="w-[12rem] h-6 rounded-l-md bg-[F5F3F4] pl-2 text-[1rem] font-[400] text-[#555] form-control border-2 border-gray-700 focus:outline-none" maxLength="50"
                 ></input>
                 <ButtonSearchModal></ButtonSearchModal>
             </div>
