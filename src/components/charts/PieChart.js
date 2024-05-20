2798e4ced28c4e3424101392371ac0c1c570a955
Kauanny Tenorio <kakau24112004@hotmail.com>
2024-05-19 22:45:00 -0300

    Ajustando os gráficos no dashboard de loja

--- src/components/charts/PieChart.js
+++ src/components/charts/PieChart.js
@@ -1,12 +1,9 @@
-import { Height } from '@mui/icons-material';
 import React from 'react';
 import Chart from 'react-apexcharts';
 
 function PieChart({ series, labels }) {
     const options = {
         chart: {
-            width: '50%',
-            // height: '100%',
             type: 'pie',
             toolbar: {
                 show: true
@@ -27,19 +24,21 @@ function PieChart({ series, labels }) {
         },
         dataLabels: {
             formatter(val, opts) {
-                const name = opts.w.globals.labels[opts.seriesIndex]
-                return [name, val.toFixed(1) + '%']
+                const name = opts.w.globals.labels[opts.seriesIndex];
+                return [name, val.toFixed(1) + '%'];
             }
         },
         legend: {
             show: true,
             position: 'right',
-           textAlign: 'right'
+            textAlign: 'right'
         }
     };
 
     return (
-        <Chart options={options} series={series} type="pie" height={'120%'} />
+        <div className="w-full h-full">
+            <Chart options={options} series={series} type="pie" height="100%" width="100%" />
+        </div>
     );
 }
 
