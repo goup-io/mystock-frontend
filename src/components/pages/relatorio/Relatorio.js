80e4ce1382c58d8f447e957a2297e8546c903a0e
Kauanny Tenorio <kakau24112004@hotmail.com>
2024-06-19 13:27:46 -0300

    Resolvendo erro

--- src/components/pages/relatorio/Relatorio.js
+++ src/components/pages/relatorio/Relatorio.js
@@ -6,7 +6,7 @@ import ButtonModal from '../../buttons/buttonsModal.js'
 import React, { useState } from 'react';
 
 //RELATORIO
-import RelatorioGeral from '../../pdf/RelatorioGeral.js'
+import RelatorioGeral from '../../pdf/RelatorioGeral.js';
 import pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
 import htmlToPdfmake from 'html-to-pdfmake';
